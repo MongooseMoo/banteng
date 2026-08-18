1f58c7a5d4cf55990da4a0b004ff957280addd2c
Christopher Toth <q.alpha@gmail.com>
2026-08-18 16:00:55 -0600

    codex: expose PR #50 test diagnostics

--- rewrite-recipes/build.gradle.kts
+++ rewrite-recipes/build.gradle.kts
@@ -36,4 +36,8 @@ tasks.withType<JavaCompile>().configureEach {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showStandardStreams = true
+    }
 }
